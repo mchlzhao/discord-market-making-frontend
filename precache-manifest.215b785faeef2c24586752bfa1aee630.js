c559269c075a07f6f997b410aac456b02828e0a4
Michael Zhao <mzhaoau+1@gmail.com>
2020-10-06 22:22:44 +1100

    Updates

---
+++ precache-manifest.215b785faeef2c24586752bfa1aee630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af34eabe336a30de30666ec1974ad885",
+    "url": "/discord-market-making-frontend/index.html"
+  },
+  {
+    "revision": "154a7cbb6d3519063df6",
+    "url": "/discord-market-making-frontend/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "7f3d4f11588ef16624cb",
+    "url": "/discord-market-making-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "154a7cbb6d3519063df6",
+    "url": "/discord-market-making-frontend/static/js/2.bb0112f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/discord-market-making-frontend/static/js/2.bb0112f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3d4f11588ef16624cb",
+    "url": "/discord-market-making-frontend/static/js/main.ad657a9d.chunk.js"
+  },
+  {
+    "revision": "9e5b729fe5791564dd64",
+    "url": "/discord-market-making-frontend/static/js/runtime-main.3f610ed4.js"
+  }
+]);
