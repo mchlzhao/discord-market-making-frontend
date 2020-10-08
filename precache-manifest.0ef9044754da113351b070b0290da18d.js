84cdb66de331225860fef7bcdc8a67388edea0a1
Michael Zhao <mzhaoau+1@gmail.com>
2020-10-09 00:28:53 +1100

    Updates

---
+++ precache-manifest.0ef9044754da113351b070b0290da18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ed9df90504cb7685b385b938a917fe",
+    "url": "/discord-market-making-frontend/index.html"
+  },
+  {
+    "revision": "3efd86676faead5f02ff",
+    "url": "/discord-market-making-frontend/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "158520af720925680223",
+    "url": "/discord-market-making-frontend/static/css/main.da79fd19.chunk.css"
+  },
+  {
+    "revision": "3efd86676faead5f02ff",
+    "url": "/discord-market-making-frontend/static/js/2.ac9a9d9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-market-making-frontend/static/js/2.ac9a9d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158520af720925680223",
+    "url": "/discord-market-making-frontend/static/js/main.873f449a.chunk.js"
+  },
+  {
+    "revision": "9e5b729fe5791564dd64",
+    "url": "/discord-market-making-frontend/static/js/runtime-main.3f610ed4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mchlzhao.github.io/discord-market-making-frontend/precache-manifest.80a17c7900c0bd712347c3dd27d0815d.js"
+  "/discord-market-making-frontend/precache-manifest.0ef9044754da113351b070b0290da18d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mchlzhao.github.io/discord-market-making-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/discord-market-making-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
